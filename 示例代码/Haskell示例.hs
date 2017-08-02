4bdfad4f88731407fca7a954e83d8f310d354b49
Tesla Ice Zhang <ice1000@kotliner.cn>
2017-08-03 01:33:29 +0800

    Update Haskell示例.hs

--- 示例代码/Haskell示例.hs
+++ 示例代码/Haskell示例.hs
@@ -1,9 +1,15 @@
 -- $ ghc -o 示例 Haskell示例.hs
 -- $ ./示例
--- $ ghc --version
--- The Glorious Glasgow Haskell Compilation System, version 8.0.2
+-- $ ghci --version
+-- The Glorious Glasgow Haskell Compilation System, version 8.2.1
 
-阶乘 0 = 1
-阶乘 数 = 数 * 阶乘 (数-1)
- 
-main = print (阶乘 5)
+右折叠 :: Foldable 可折叠 => (甲 -> 乙 -> 乙) -> 乙 -> 可折叠 甲 -> 乙
+右折叠 = foldr
+
+乘 :: Num 数值 => 数值 -> 数值 -> 数值
+乘 = (*)
+
+阶乘 数 = 右折叠 乘 1 [ 1 .. 数 ]
+
+main :: IO ()
+main = print $ 阶乘 5
