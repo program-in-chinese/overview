5c48b9c0a1dfb93088a5b65c79724c3e38c6d8e0
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-08-04 23:29:32 -0700

    swift例程

---
+++ 示例代码/变量.swift
@@ -0,0 +1,12 @@
+// $ swift 变量.swift
+// $ swiftc --version
+// Apple Swift version 1.2 (swiftlang-602.0.53.1 clang-602.0.53)
+// Target: x86_64-apple-darwin14.5.0
+
+var 姓名 = "阿蕾"
+
+var 年龄: Int; 年龄 = 21
+
+var 身高: Int = 180
+
+print("姓名: \(姓名), 年龄: \(年龄) & 身高: \(身高)")
