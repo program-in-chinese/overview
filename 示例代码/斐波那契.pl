007beaaea5805d3ddc313630ccdb2baf7d62da2d
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-10-26 01:15:16 -0700

    Perl示例:中文化变量

--- 示例代码/斐波那契.pl
+++ 示例代码/斐波那契.pl
@@ -6,10 +6,10 @@ use utf8;
 
 sub 斐波那契
 {
-    my ($rec) = @_;
-    return 0 if $rec == 0;
-    return 1 if $rec == 1;
-    return 斐波那契($rec - 1) + 斐波那契($rec - 2);
+    my ($数) = @_;
+    return 0 if $数 == 0;
+    return 1 if $数 == 1;
+    return 斐波那契($数 - 1) + 斐波那契($数 - 2);
 }
 
 print &斐波那契(16)
