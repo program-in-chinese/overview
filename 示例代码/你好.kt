a5c7793612febb09a9411f8d62afdc754b6f8746
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-11-10 10:07:25 -0800

    #49 修正运行步骤, 输入提示

--- 示例代码/你好.kt
+++ 示例代码/你好.kt
@@ -1,8 +1,8 @@
-// $ kotlinc 你好.kt -include-runtime -d 你好.jar
+// $ kotlinc 你好.kt -cp junit.jar:kotlin-test-1.1.51.jar -include-runtime -d 你好.jar
 // $ java -jar 你好.jar
 
 // $ kotlinc -version
-// info: kotlinc-jvm 1.1.3-2 (JRE 1.8.0_131-amd64)
+// info: kotlinc-jvm 1.1.51 (JRE 1.8.0_45-b14)
 
 import org.junit.Assert
 import java.util.*
@@ -12,7 +12,7 @@ typealias 扫描器 = Scanner
 typealias 系统 = System
 
 fun main(vararg 参数: String) {
-	打印("你好，我可爱吧!")
+	打印("你好，我可爱吧! 下面请输入数字, 不然原地爆炸")
 	val 我是一个变量 = 扫描器(系统.`in`).nextInt()
 }
 
