a1629f7cad3d4f71e4da0ce3b7c22a045e408311
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-03-26 22:18:53 -0700

    新添

---
+++ qq群/分享代码/测试_buyouyuan.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define pi 45
+int main()
+{
+ 
+int a = 3;
+int 回1=10;
+int 订;
+int i;
+
+printf("hello!\n");
+回(i=0;i<5;i++)
+{
+	
+	订=a+pi+回1;
+	printf("订=%d\n",订);
+	回1++;
+}
+do
+{
+ 回1 = 回1 + 20;
+ printf("回1=%d\n",回1);
+}圆(回1<100);
+
+return 0;
+}
