a2be06dd2ec7748fcd45600594d720844085fb88
fromwheretowhere <fromwheretowhere.service@gmail.com>
2020-05-06 20:45:16 -0700

    清理;

--- 示例代码/斐波那契.py
+++ 示例代码/斐波那契.py
@@ -2,9 +2,9 @@
 
 def 斐波那契(数):
     if 数 < 2:
-        return 1;
+        return 1
     else:
-        return 斐波那契(数 - 2) + 斐波那契(数 - 1);
+        return 斐波那契(数 - 2) + 斐波那契(数 - 1)
      
 for 索引 in range(1,16):
-    print(斐波那契(索引));
+    print(斐波那契(索引))
